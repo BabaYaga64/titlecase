05b9518da5e4113452952b76802738ea6af9e684
Bojana Skarich & Liz Beacham <student@epicodus.com>
2015-04-07 11:25:53 -0700

    Add join method in script.js to put all returned array items in one string.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-var titleCase = function(word) {
-    return word.charAt(0).toUpperCase() + word.slice(1);
+function titleCase(string) {
+    var split_words = string.split(" ");
+    var word_list = [];
+
+    for (var i = 0; i < split_words.length; i += 1) {
+        var word = split_words[i].charAt(0).toUpperCase() + split_words[i].slice(1) ;
+        word_list.push(word);
+    }
+    //return word_list ??
+
+    var titleCasedString = word_list.join(" ");
+    return titleCasedString;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,12 @@ describe("titleCase", function() {
     it("capitalizes the first letter of a word", function() {
         expect(titleCase("cat")).to.equal("Cat");
     });
+
+    it("keeps the first letter of word capitalized if it already is capitalized", function() {
+        expect(titleCase("Cat")).to.equal("Cat");
+    });
+
+    it("capitalizes the first letter of each word in a two-word string", function() {
+        expect(titleCase("cat hat")).to.equal("Cat Hat");
+        });
 });
