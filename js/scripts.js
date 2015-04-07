08faf720c25f26917754f971c0e8a47fc0e32da3
Bojana Skarich & Liz Beacham <student@epicodus.com>
2015-04-07 14:10:27 -0700

    Refactor script to capitalize first character in returned string.

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,23 @@ function titleCase(string) {
     var word_list = [];
 
     for (var i = 0; i < split_words.length; i += 1) {
-        var word = split_words[i].charAt(0).toUpperCase() + split_words[i].slice(1) ;
-        word_list.push(word);
-    }
-    //return word_list ??
 
-    var titleCasedString = word_list.join(" ");
-    return titleCasedString;
+        if (split_words[i] === "the" || split_words[i] === "a" || split_words[i] === "an" || split_words[i] === "with" || split_words[i] === "and") {
+            word_list.push(split_words[i]);
+        }
+
+        else {
+            var word = split_words[i].charAt(0).toUpperCase() + split_words[i].slice(1) ;
+            word_list.push(word);
+        }
+    }
+
+    var title_cased_string = word_list.join(" ");
+
+    var final_string = title_cased_string.charAt(0).toUpperCase() + title_cased_string.slice(1);
+
+    return final_string;
 };
+
+var string = "the the cat an a cat hat with and";
+console.log(titleCase(string));
